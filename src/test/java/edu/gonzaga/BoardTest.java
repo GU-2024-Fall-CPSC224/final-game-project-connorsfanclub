5ee957f4b20babbdbc9d272727d07c67d9a7190a
devyn hubbs <dhubbs2@zagmail.gonzaga.edu>
2025-04-22 10:03:45 -0700

    test layout

---
+++ src/test/java/edu/gonzaga/BoardTest.java
@@ -0,0 +1,27 @@
+package edu.gonzaga;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    
+    @Test
+    void testInitializeBoard() {
+
+    }
+    
+    @Test 
+    void testIsEnd(int index) {
+
+    }
+
+    @Test 
+    void testGetSymbolIndex(String symbol) {
+
+    }
+
+    @Test 
+    void testMovePlayerToken(Player player, Card card) {
+
+    }
+
+}
---
+++ src/test/java/edu/gonzaga/CardDeckTest.java
@@ -0,0 +1,22 @@
+package edu.gonzaga;
+
+import org.junit.jupiter.api.Test;
+
+public class CardDeckTest {
+    
+    @Test 
+    void testInitializeDeck() {
+
+    }
+
+    @Test 
+    void testShuffle() {
+
+    }
+
+    @Test 
+    void testDrawCard() {
+
+    }
+
+}
