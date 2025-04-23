7345cc933c33de5ba93b154dd1be71f923c9ffe5
cdoebler25 <164575098+cdoebler25@users.noreply.github.com>
2025-04-23 11:34:15 -0700

    Sets up GUI File

--- src/main/java/edu/gonzaga/SplashScreen.java
+++ src/main/java/edu/gonzaga/SplashScreen.java
@@ -1,3 +1,28 @@
 // this could be where we write our Java Swing screen for ntro?
 // I don't know if we want another file like GameGUI.java for the board implementation?
 package edu.gonzaga;
+import javax.swing.JFrame;
+public class SplashScreen extends JFrame{ 
+    //this is where the Graphic User interface will go
+    JFrame frame; 
+    
+
+    public SplashScreen(){
+
+        frame= new JFrame("GU Candy Land"); 
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
